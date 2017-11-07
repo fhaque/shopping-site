085a1625d64bd294552d976af5c8ad14dfa0db89
Fatin Haque <fhaque@Fatins-MacBook-Pro.local>
2017-11-07 10:39:02 -0500

    the observable map to item details functioning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from './models/app.model';
+import { ItemsActions } from './actions/items.actions';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  constructor(
+    ngRedux: NgRedux<IAppState>,
+    actions: ItemsActions
+  ) { 
+    ngRedux.dispatch( actions.getDefaultItems() );
+  }
+
 }
--- src/app/components/display-items/display-items.component.ts
+++ src/app/components/display-items/display-items.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { NgRedux } from '@angular-redux/store';
 import { IAppState } from '../../models/app.model';
-import { IItemList } from '../../models/items.model';
+import { IItemList, IItem } from '../../models/items.model';
 import { id } from '../../models/items.model';
 import { DisplayItemsActions } from '../../actions/display-items.actions';
 
@@ -13,14 +14,19 @@ import { DisplayItemsActions } from '../../actions/display-items.actions';
   styleUrls: ['./display-items.component.css']
 })
 export class DisplayItemsComponent {
-  readonly  items$: Observable<id>;
+  readonly  items$: Observable<IItem[]>;
 
   constructor(
     ngRedux: NgRedux<IAppState>,
     actions: DisplayItemsActions
   ) {
-    this.items$ = ngRedux.select<id>('displayItems');
-    ngRedux.dispatch( actions.addItem('25') );
+    // this.items$ = ngRedux.select<id[]>('displayItems').map( (item) => item.map(l => l + 'a') );
+    this.items$ = ngRedux.select<id[]>('displayItems').map( ids => {
+      const { itemList } = ngRedux.getState();
+      return ids.map( id => itemList.items[id] );
+    });
+    ngRedux.dispatch( actions.addItem('0') );
+    ngRedux.dispatch( actions.addItem('8ch') );
   }
 
 }
