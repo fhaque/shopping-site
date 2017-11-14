12b2cf1fdb5ec52e4dbd0115fa4ec84abaac940f
Fatin Haque <frhaque@gmail.com>
2017-11-14 18:17:49 -0500

    Added search query param to URL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,12 @@
 import { Component } from '@angular/core';
 
-import { NgRedux } from '@angular-redux/store';
-import { IAppState } from './models/app.model';
-import { ItemsActions } from './actions/items.actions';
-import { AppService } from './services/app.service';
-import { IItemsRef } from './models/items.model';
-import { Observable } from 'rxjs/Observable';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   constructor(
-    private ngRedux: NgRedux<IAppState>,
-    private actions: ItemsActions,
-    private appService: AppService
   ) {}
-
-  ngOnInit() {
-    this.ngRedux.dispatch( this.actions.getDefaultItems() );
-  }
 }
--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OnInit } from '@angular/core';
+
+import { ItemsActions } from '../../actions/items.actions';
+import { NgRedux } from '@angular-redux/store/lib/src/components/ng-redux';
+import { IAppState } from '../../models/app.model';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.css']
 })
-export class SearchResultsComponent {
+export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private itemsActions: ItemsActions,
+    private ngRedux: NgRedux<IAppState>,
+  ) { }
+
+  ngOnInit() {
+    this.route.queryParams
+      .subscribe( params => this.queryHandle(params.query || '') );
+  }
+
+  private queryHandle(query: string) {
+    if (query !== '') {
+      this.ngRedux.dispatch( this.itemsActions.getItemsByQuery(query) );
+    } else {
+      this.ngRedux.dispatch( this.itemsActions.getDefaultItems() )
+    }
+  }
 
 }
