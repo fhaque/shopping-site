29c7e46b77f23a438b2414e3143aabe9d9533e3b
Fatin Haque <frhaque@gmail.com>
2017-12-14 10:47:44 -0500

    cleaned up the user login epic spec

--- src/app/epics/user-login.epic.spec.ts
+++ src/app/epics/user-login.epic.spec.ts
@@ -15,153 +15,135 @@ import { AnyAction } from 'redux';
 import { IUser } from '../models/user.model';
 import { UserActions } from '../actions/user.actions';
 
-const mockUser: IUser = { name: 'MOCK_user' };
-
-class MockLoginService {
-    login = (username, pass) => {};
-
-    logout = () => {};
-}
-// TODO: Don't feel confident in the testing of Observables.
-// TODO: Needs to be updated since Login Service has changed its error handle.
-//      And because there is now a Router dependency
-class MockUserActions {
-    loginSuccess = (user, date) => ({ type: 'MOCK_LOGIN_SUCCESS', user, date });
-    loginFailed = (err) => ({ type: 'MOCK_LOGIN_ERROR', err });
-}
-
-class MockRouter {
-    router = {
-        navigate: (commands, extras) => {},
-    };
-}
-
 
 describe('UserLoginEpic', () => {
+    const mockUser: IUser = { name: 'MOCK_user' };
+
     let mockUserActions: any;
     let mockLoginService: any;
     let mockRouter: any;
     let userLoginEpic: UserLoginEpic;
     let action$: any;
+    let userLoginEpicFactory: (loginService: any, userActions: any) =>
+      UserLoginEpic;
 
+    beforeEach(() => {
+        mockRouter = {};
+        mockRouter.router = jasmine.createSpyObj('router', ['navigate']);
 
-    describe('login', () => {
-        beforeEach(() => {
-            mockLoginService = new MockLoginService();
-            mockUserActions = new MockUserActions();
-            mockRouter = new MockRouter();
-            userLoginEpic = new UserLoginEpic(
-                mockLoginService as LoginService,
-                mockUserActions as UserActions,
+        userLoginEpicFactory = (loginService, userActions) =>
+            new UserLoginEpic(
+                loginService as LoginService,
+                userActions as UserActions,
                 mockRouter as Router
             );
-        });
+    });
 
-        it('should initiate login and succeed', (done: DoneFn) => {
-            const user = 'USER';
-            const pass = 'PASSWORD';
-            const date: number = Date.now();
+    describe('login', () => {
+        const user = 'USER';
+        const pass = 'PASSWORD';
+        const date: number = Date.now();
+        const err = 'Mock Error';
+        const mockLoginSuccessAction = {
+            type: 'MOCK_LOGIN_SUCCESS',
+            user,
+            date,
+        };
+        const mockLoginFailedAction = {
+            type: 'MOCK_LOGIN_ERROR',
+            err
+        };
 
-            spyOn(mockLoginService, 'login')
-                .and
-                .returnValue( Observable.of(mockUser) );
-            spyOn(mockUserActions, 'loginSuccess')
-                .and
-                .returnValue( mockUserActions.loginSuccess(user, date) );
-            spyOn(mockUserActions, 'loginFailed')
-                .and
-                .returnValue( mockUserActions.loginFailed('Mock Error') )
-
+        beforeEach(() => {
             action$ = ActionsObservable.of({
                 type: UserActions.LOGIN_USER_REQUEST,
                 username: user,
                 pass: pass,
             });
-
-            userLoginEpic.login(action$)
-                .subscribe(
-                    () => {},
-
-                    () => done.fail('User Login Epic: login fail'),
-
-                    () => {
-                        expect( mockLoginService.login )
-                            .toHaveBeenCalledWith(user, pass);
-                        expect( mockUserActions.loginSuccess )
-                            .toHaveBeenCalledWith(user, date);
-                        expect( mockUserActions.loginFailed )
-                            .not.toHaveBeenCalled();
-                        done();
-                    }
-                );
         });
-        it('should throw error and failed action when login fails', (done: DoneFn) => {
-            const user = 'cheese';
-            const pass = 'cheese';
-            const date: number = Date.now();
-            const mockErrorMsg = 'MOCK_ERROR';
 
-            spyOn(mockLoginService, 'login')
-                .and
-                .returnValue( Observable.throw(mockErrorMsg) );
-            spyOn(mockUserActions, 'loginSuccess')
-                .and
-                .returnValue( mockUserActions.loginSuccess(user, date) );
-            spyOn(mockUserActions, 'loginFailed')
-                .and
-                .returnValue( mockUserActions.loginFailed(mockErrorMsg) );
-
-            action$ = ActionsObservable.of({
-                type: UserActions.LOGIN_USER_REQUEST,
-                username: user,
-                pass: pass,
+        it('should initiate login with login request', (done: DoneFn) => {
+            mockLoginService = jasmine.createSpyObj('loginService', {
+                login: Observable.of(mockUser),
             });
+            mockUserActions = jasmine
+                .createSpyObj('userActions', ['loginSuccess', 'loginFailed']);
 
-            userLoginEpic.login(action$)
-            .subscribe(
-                () => {},
-
-                () => done.fail('User Login Epic: login fail'),
-
-                () => {
-                    expect( mockLoginService.login )
-                        .toHaveBeenCalledWith(user, pass);
-                    expect( mockUserActions.loginFailed )
-                        .toHaveBeenCalledWith(mockErrorMsg);
-                    expect( mockUserActions.loginSuccess )
-                        .not.toHaveBeenCalled();
-                    done();
-                }
+            userLoginEpic = userLoginEpicFactory(
+                mockLoginService,
+                mockUserActions
             );
 
+            userLoginEpic
+                .login(action$)
+                .subscribe( action => {
+                    expect( mockLoginService.login )
+                        .toHaveBeenCalledWith(user, pass);
+                    done();
+                });
+        });
 
+        it('should emit login success action with login service success',
+            (done: DoneFn) => {
+                mockLoginService = { login: () => Observable.of(mockUser) };
+                mockUserActions = { loginSuccess: () =>
+                  mockLoginSuccessAction
+                };
+
+                userLoginEpic = userLoginEpicFactory(
+                    mockLoginService,
+                    mockUserActions
+                );
+
+                userLoginEpic
+                    .login(action$)
+                    .subscribe( action => {
+                        expect( action ).toEqual( mockLoginSuccessAction );
+                        done();
+                    });
+            }
+      );
+
+        it('should emit failed login action if login fails', (done: DoneFn) => {
+            mockLoginService = { login: () => Observable.throw(err) };
+            mockUserActions = { loginFailed: () => mockLoginFailedAction };
+
+            userLoginEpic = userLoginEpicFactory(
+                mockLoginService,
+                mockUserActions
+            );
+
+            userLoginEpic
+            .login(action$)
+            .subscribe( action => {
+                expect( action ).toEqual( mockLoginFailedAction );
+                done();
+            });
         });
 
     });
 
     describe('logout', () => {
         beforeEach(() => {
-            mockLoginService = new MockLoginService();
-            mockUserActions = new MockUserActions();
-            mockRouter = new MockRouter();
-            userLoginEpic = new UserLoginEpic(
-                mockLoginService as LoginService,
-                mockUserActions as UserActions,
-                mockRouter as Router
-            );
-        });
-        it('should initiate logout and emit no action', (done: DoneFn) => {
-            spyOn(mockLoginService, 'logout');
-
             action$ = ActionsObservable.of({
                 type: UserActions.LOGOUT_USER,
             });
+        });
+
+        it('should initiate logout with logout request', (done: DoneFn) => {
+            mockLoginService = jasmine.createSpyObj('loginService', {
+                logout: undefined,
+            });
+            mockUserActions = {};
+
+            userLoginEpic = userLoginEpicFactory(
+                mockLoginService,
+                mockUserActions
+            );
 
             userLoginEpic.logout(action$)
                 .subscribe(
-                    (val) => {
-                        expect( val ).not.toBeDefined();
-                    },
+                    () => {},
 
                     () => done.fail('User Login Epic: logout fail'),
 
@@ -170,6 +152,25 @@ describe('UserLoginEpic', () => {
                             .toHaveBeenCalled();
                         done();
                     }
+                );
+        });
+        it('should emit no action upon logging out', (done: DoneFn) => {
+            mockLoginService = { logout: () => {} };
+            mockUserActions = {};
+
+            userLoginEpic = userLoginEpicFactory(
+                mockLoginService,
+                mockUserActions
+            );
+
+            userLoginEpic.logout(action$)
+                .subscribe(
+                    (val) => {
+                      expect( val ).not.toBeDefined();
+                    },
+
+                    () => done.fail('User Login Epic: logout fail'),
+                    () => done()
                 );
         });
     });
